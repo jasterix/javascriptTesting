8177eeddaa009d5a4fd6c66e0d3cc6601e62a5bc
Jasterix <ljcalixte@gmail.com>
2020-03-18 08:52:02 -0400

    complete isNull test

--- functions.test.js
+++ functions.test.js
@@ -4,6 +4,6 @@ test("Adds 2+2 to equal 4", () => {
   expect(functions.add(2, 2)).toBe(4);
 });
 
-test("Adds 2+2 to NOT equal 5", () => {
-  expect(functions.add(2, 2)).not.toBe(4);
+test("Should be null", () => {
+  expect(functions.isNull()).toBeNull();
 });
