fd904918a44b4f0e528f45903e47c1c589c39749
Jasterix <ljcalixte@gmail.com>
2020-03-18 09:22:23 -0400

    create jasterix object and load test

--- functions.js
+++ functions.js
@@ -1,7 +1,15 @@
 const functions = {
   add: (num1, num2) => num1 + num2,
   isNull: () => null,
-  checkValue: x => x
+  checkValue: x => x,
+
+  createUser: () => {
+    const user = {
+      firstName: "Jasterix"
+    };
+    user["lastName"] = "Obelix";
+    return user;
+  }
 };
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -15,3 +15,18 @@ test("Should be falsy", () => {
 test("Should be truthy", () => {
   expect(functions.checkValue(2)).toBeTruthy();
 });
+
+// user object
+// toBe is for primitive types. Objects and arrays are reference types meaning they're saved in different memory areas
+test("User should be Jasterix Obelix object", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "Jasterix",
+    lastName: "Obelix"
+  });
+});
+
+test("Should be under 1600", () => {
+  const load1 = 800;
+  const load2 = 600;
+  expect(load1 + load2).toBeLessThan(1600);
+});
